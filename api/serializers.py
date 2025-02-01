77202699cb96b68ac98544bfbc418dad7808d049
tussanakorn <tussanakorn2000@gmail.com>
2025-02-02 00:40:43 +0700

    Add RegisterSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -20,3 +20,23 @@ class WorkExperienceSerializer(serializers.ModelSerializer):
     class Meta:
         model = WorkExperience
         fields = '__all__'
+        
+class RegisterSerializer(serializers.ModelSerializer):
+    tel = serializers.CharField(max_length=15, required=False)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'first_name', 'last_name', 'email', 'tel')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', ''),
+            email=validated_data.get('email', ''),
+        )
+        user.profile.tel = validated_data.get('tel', '')
+        user.profile.save()
+        return user
