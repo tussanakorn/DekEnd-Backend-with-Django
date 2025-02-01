e826e09e43c3ca7337c665fe46d25dd45a78be4e
tussanakorn <tussanakorn2000@gmail.com>
2025-02-02 01:03:00 +0700

    Update urlpatterns

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import InternViewSet, EducationViewSet, TrainingViewSet, WorkExperienceViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import InternViewSet, EducationViewSet, TrainingViewSet, WorkExperienceViewSet, RegisterView, LogoutView
 
 router = DefaultRouter()
 router.register(r'interns', InternViewSet)
@@ -9,5 +10,9 @@ router.register(r'trainings', TrainingViewSet)
 router.register(r'work-experiences', WorkExperienceViewSet)
 
 urlpatterns = [
+    path('auth/register/', RegisterView.as_view(), name='auth_register'),
+    path('auth/login/', TokenObtainPairView.as_view(), name='auth_login'),
+    path('auth/refresh/', TokenRefreshView.as_view(), name='auth_refresh'),
+    path('auth/logout/', LogoutView.as_view(), name='auth_logout'),
     path('', include(router.urls)),
 ]
