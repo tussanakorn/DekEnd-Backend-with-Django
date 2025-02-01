f0269ad3eba95fa712e18971641240030aaf9607
tussanakorn <tussanakorn2000@gmail.com>
2025-02-02 00:03:20 +0700

    Add api and url

--- api/views.py
+++ api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Intern, Education, Training, WorkExperience
+from .serializers import InternSerializer, EducationSerializer, TrainingSerializer, WorkExperienceSerializer
 
-# Create your views here.
+class InternViewSet(viewsets.ModelViewSet):
+    queryset = Intern.objects.all()
+    serializer_class = InternSerializer
+    
+class EducationViewSet(viewsets.ModelViewSet):
+    queryset = Education.objects.all()
+    serializer_class = EducationSerializer
+    
+class TrainingViewSet(viewsets.ModelViewSet):
+    queryset = Training.objects.all()
+    serializer_class = TrainingSerializer
+    
+class WorkExperienceViewSet(viewsets.ModelViewSet):
+    queryset = WorkExperience.objects.all()
+    serializer_class = WorkExperienceSerializer
